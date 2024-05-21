9782d79a42fc1d7e6f6ddaf5dcfc960526142493
Jayesh033 <jayeshrevekar3@gmail.com>
2024-05-21 20:05:27 +0530

    SEMPI Website

---
+++ newscript.js
@@ -0,0 +1,17 @@
+const options = {
+    bottom: '32px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#D3D3D3', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: false // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
